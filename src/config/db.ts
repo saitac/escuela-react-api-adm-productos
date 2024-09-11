faf80f55dca7399fffcac4e88eb893f4f3582106
salvador <salvadoraita@gmail.com>
2024-09-11 15:01:05 -0300

    20240911 15:00

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,19 @@
 
 import {Sequelize} from "sequelize"
+import dotenv from "dotenv"
 
-const db = new Sequelize("postgresql://escuela_rect_api_adm_productos_user:aqQwbyU7rKiKPoFFHg9mLd7jZao6zD7t@dpg-crg9val6l47c73dttbqg-a.oregon-postgres.render.com/escuela_rect_api_adm_productos");
+dotenv.config();
+
+//const db = new Sequelize("postgresql://escuela_rect_api_adm_productos_user:aqQwbyU7rKiKPoFFHg9mLd7jZao6zD7t@dpg-crg9val6l47c73dttbqg-a.oregon-postgres.render.com/escuela_rect_api_adm_productos?ssl=true");
+
+const db = new Sequelize(<string>process.env.DATABASE_URL,
+    {
+        dialectOptions: {
+            ssl: {
+                require: false
+            }
+        }
+    }
+);
 
 export default db;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 
 import express, {Express} from "express";
 import generalRouter from "./routers/general";
+import db from "./config/db";
+
+// Función que conecta a la BD
+const connectDB = async () => {
+    try {
+        await db.authenticate();
+        console.log("Conexión existosa a la BD");
+    } catch (error) {
+        console.log(error);
+        console.log("Hubo un error al conectar a la BD");
+    }
+}
+
+connectDB();
 
 const server: Express = express();
 
